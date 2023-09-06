3d735266d0c41ff297e0566692766df9c60ad21d
danil moskalenko <dextwelwe@gmail.com>
2023-09-06 16:33:42 -0400

    page se connecter / inscrire html+css

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import UtilisateursMain from './components/utilisateurs/utilisateursMain';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <>
-  
+    <UtilisateursMain></UtilisateursMain>
     </>
   )
 }

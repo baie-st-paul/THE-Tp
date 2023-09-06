6b40038aa31ec0c112e54a5b4a2044ffdc845c94
Philippe Vallerand <pvallerand1@gmail.com>
2023-09-06 14:42:58 -0400

    Enlever le plugin Spring security temporairement

--- Tp-Backend/src/main/java/com/example/tpbackend/TpBackendApplication.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/TpBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TpBackendApplication  implements CommandLineRunner {
+public class TpBackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TpBackendApplication.class, args);

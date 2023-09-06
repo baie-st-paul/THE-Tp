6e1b63529f3118b5f5ab25380ceef36dc879927d
danil moskalenko <dextwelwe@gmail.com>
2023-09-06 15:22:27 -0400

    ajout dossier

---
+++ my-react-app/src/components/utilisateurs/inscription/inscriptionEtudiant.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function inscriptionEtudiant() {
+  return (
+    <div>inscriptionEtudiant</div>
+  )
+}
---
+++ my-react-app/src/components/utilisateurs/utilisateurs_main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import al from  '../images/al.png' 
+
+export default function Utilisateurs_main() {
+
+
+
+
+
+  return (
+   <>
+
+
+   </>
+  )
+}

a9e10be7c64f3899efadae1afc613959d260d128
Philippe Vallerand <pvallerand1@gmail.com>
2023-09-06 11:32:56 -0400

    Initialization du projet Spring pour la backend

---
+++ Tp-Backend/src/main/java/com/example/tpbackend/TpBackendApplication.java
@@ -0,0 +1,19 @@
+package com.example.tpbackend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBackendApplication  implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpBackendApplication.class, args);
+    }
+    @Override
+    public void run(String... args) {
+        System.out.println("hello");
+        System.out.println();
+
+    }
+}

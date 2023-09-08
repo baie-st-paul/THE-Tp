f7bb527979d5d6a7d240093567ea6cca5257adc1
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-09-07 21:00:33 -0400

    EQ2-1 : gestion de l'insciption de l'étudiant

--- Tp-Backend/src/main/java/com/example/tpbackend/repository/StudentRepository.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/repository/StudentRepository.java
@@ -2,11 +2,17 @@ package com.example.tpbackend.repository;
 import com.example.tpbackend.DTO.PostDTO.StudentPostDTO;
 import com.example.tpbackend.models.utilisateur.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
+    @Query("SELECT COUNT(s) > 0 FROM Student s WHERE s.matricule = ?1 OR s.utilisateur.email = ?2")
+    boolean existsByMatriculeOrEmail(String matricule, String email);
+
+
+
 
 }

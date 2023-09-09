97ccec02cbdba844012ffe8a71b7a84114458118
lina <2033162@claurendeau.qc.ca>
2023-09-08 22:45:02 -0400

    ajustement des nom de variable pour que lien BE - FE fonctionne

--- my-react-app/src/components/utilisateurs/inscription/etudiant/InscriptionPage.js
+++ my-react-app/src/components/utilisateurs/inscription/etudiant/InscriptionPage.js
@@ -18,6 +18,7 @@ const InscriptionPage = () => {
             })
         const data = await res.json()
         setEtudiants([...etudiants, data])
+        console.log(data)
     }
 
     return (
@@ -28,9 +29,9 @@ const InscriptionPage = () => {
             {<InscriptionForm onAdd={inscription}/>}
             <Link to='/' className='centrerPage pt-2'>Retour</Link>
             {
-                etudiants.length > 0 ?
+                /*etudiants.length > 0 ?
                     <Navigate to="/"/>
-                    : console.log('nothing yet')
+                    : console.log('nothing yet')*/
             }
             <div >
                 {<InscriptionForm />}

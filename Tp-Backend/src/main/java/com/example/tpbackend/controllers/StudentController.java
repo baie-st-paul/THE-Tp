08670710a52e927a0cfddecb100666d30c6306b1
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-09-13 00:59:49 -0400

    refactor

--- Tp-Backend/src/main/java/com/example/tpbackend/controllers/StudentController.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/controllers/StudentController.java
@@ -9,12 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/stages")
+@RequestMapping("/api/v1/stages/student-signup")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     StudentServices studentServices;

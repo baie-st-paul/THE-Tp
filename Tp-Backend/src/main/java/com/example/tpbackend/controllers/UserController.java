f2c974a39d2d8632e8eacaeed06ad8f2f92254b1
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-09-13 00:59:49 -0400

    refactor

--- Tp-Backend/src/main/java/com/example/tpbackend/controllers/UserController.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/controllers/UserController.java
@@ -6,15 +6,13 @@ import com.example.tpbackend.repository.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/stages")
+@CrossOrigin(origins = "*")
 
 public class UserController {
     private  final UserService userService;

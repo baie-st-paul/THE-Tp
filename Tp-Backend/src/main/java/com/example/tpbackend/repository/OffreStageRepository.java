c7c4ca633b56878435413f2d39b4da5bf4f1719c
lina <2033162@claurendeau.qc.ca>
2023-09-27 10:50:33 -0400

    EQ2-80 - FE, finalisation du veto, un peu de BE

--- Tp-Backend/src/main/java/com/example/tpbackend/repository/OffreStageRepository.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/repository/OffreStageRepository.java
@@ -1,7 +1,10 @@
 package com.example.tpbackend.repository;
 
 import com.example.tpbackend.models.OffreStage;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +13,10 @@ import java.util.Optional;
 @Repository
 public interface OffreStageRepository extends JpaRepository<OffreStage, Integer> {
     Optional<OffreStage> findOffreById(Long id);
-    List<OffreStage> findAllByTitre(String titre);
+    List<OffreStage> findAllByStudentProgram(String program);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE OffreStage SET status = ?2 WHERE titre = ?1")
+    void updateOffreStatusByTitre(String titre, OffreStage.Status statusOffre);
 }

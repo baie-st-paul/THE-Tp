f5488eaa00ee83cc21d1f2e4e58eaa8a0f7180b3
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-09-28 14:25:03 -0400

    amelioration student home page

--- my-react-app/src/components/landingPage/StudentHomePage/StudentHomePage.js
+++ my-react-app/src/components/landingPage/StudentHomePage/StudentHomePage.js
@@ -6,6 +6,8 @@ import {Nav, Navbar} from "react-bootstrap";
 import OffresPageStudent from "../offresStages/OffrePageStudent";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileUpload, faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+
 
 const StudentHomePage = () => {
     const { loggedInUser, setLoggedInUser } = useUser();
@@ -31,6 +33,20 @@ const StudentHomePage = () => {
         setActiveContent(content);
     };
 
+    const articles = [
+      "Les avantages des stages",
+      "Comment réussir votre entretien",
+      "Les meilleures entreprises pour les stages à Montréal",
+      "Développer vos compétences en programmation",
+      "Comprendre les bases de l'IA",
+      "La cybersécurité : Ce que chaque étudiant devrait savoir",
+      "Travailler dans un environnement Agile",
+      "Comment préparer un portfolio de développeur",
+      "Éviter le burnout pendant un stage",
+      "Networking : Pourquoi et comment"
+  ];
+  
+
     switch (activeContent) {
         case "file-uploader":
             contentToRender = <FileUploader matricule={matricule} />;
@@ -39,8 +55,37 @@ const StudentHomePage = () => {
             contentToRender = <OffresPageStudent/>;
             break;
         default:
-            contentToRender = <div>Select an action.</div>;
-            break;
+          contentToRender = (
+            <Container fluid>
+              <Row>
+                <Col xs={4}>
+                  <div className="sidebar">
+                    <ListGroup>
+                      {articles.map((article, index) => (
+                        <ListGroup.Item action href={`#article-${index + 1}`} key={index}>
+                          {article}
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </div>
+                </Col>
+                <Col xs={8}>
+                  {articles.map((article, index) => (
+                    <Card className="mb-4" key={index} id={`article-${index + 1}`}>
+                      <Card.Header as="h1">{article}</Card.Header>
+                      <Card.Body>
+                        <Card.Title as="h3">Introduction</Card.Title>
+                        <Card.Text className="light-bg">
+                          Contenu de {article}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  ))}
+                </Col>
+              </Row>
+            </Container>
+          );
+          break;
     }
 
     return (
@@ -49,7 +94,7 @@ const StudentHomePage = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
-                <button className="nav-item nav-button " style={{ marginRight: '20px' }} onClick={() => setActiveContent('file-uploader')}>
+                <button className="nav-item " style={{ marginRight: '20px' }} onClick={() => setActiveContent('file-uploader')}>
                   <FontAwesomeIcon icon={faFileUpload} style={{ marginRight: '10px' }}/>CV
                 </button>
                 <button className="nav-item nav-button mr-3" onClick={() => handleButtonClick('offre-page-student')}>

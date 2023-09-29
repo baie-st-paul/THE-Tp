1b13b06c59e9ea95cd66312e18c5a056ace657d8
Moukaïla <60848744+moukailay@users.noreply.github.com>
2023-09-29 19:09:50 -0400

    WIP

---
+++ my-react-app/src/components/landingPage/StudentHomePage/ArticleStudentHomePage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const ArticleStudentHomePage = () => {
+    
+    return (
+        <div className="article-container">
+            <h1>Les Stages en Informatique pour Étudiants à Montréal</h1>
+
+            <section>
+                <h2>Introduction</h2>
+                <p>
+                    Faire un stage en informatique à Montréal peut être une expérience enrichissante et bénéfique pour chaque étudiant. Cette article couvrira les opportunités, les perspectives et les statistiques sur le marché des stages à Montréal.
+                </p>
+            </section>
+
+            <section>
+                <h2>Opportunités</h2>
+                <p>
+                    Montréal est l'un des pôles technologiques du Canada, avec un grand nombre d'entreprises en démarrage, de moyennes entreprises et de grandes corporations. Les opportunités de stages abondent dans des domaines tels que le développement Web, la science des données, l'intelligence artificielle et bien plus encore.
+                </p>
+            </section>
+
+            <section>
+                <h2>Perspectives</h2>
+                <p>
+                    Les étudiants qui effectuent des stages à Montréal ont souvent l'opportunité de travailler sur des projets innovants. Ces expériences peuvent ouvrir la porte à des opportunités d'emploi à temps plein après l'obtention du diplôme.
+                </p>
+            </section>
+
+            <section>
+                <h2>Statistiques</h2>
+                <ul>
+                    <li>Le taux de placement en stage est d'environ 85%.</li>
+                    <li>Le salaire moyen pour un stagiaire en informatique à Montréal est d'environ $20/h.</li>
+                    <li>Plus de 500 entreprises dans la région métropolitaine de Montréal proposent des stages en informatique chaque année.</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Conclusion</h2>
+                <p>
+                    Faire un stage en informatique à Montréal peut offrir de nombreuses opportunités pour l'avenir. Avec un marché en pleine croissance et une grande diversité de domaines, les étudiants ont toutes les cartes en main pour réussir leur intégration professionnelle.
+                </p>
+            </section>
+        </div>
+    );
+};
+
+export default ArticleStudentHomePage;

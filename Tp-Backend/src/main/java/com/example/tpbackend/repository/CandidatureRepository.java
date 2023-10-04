695fbdda68133e35017d8b553fbf51901430cf01
Ivan Jimenez <ivanjr1802@gmail.com>
2023-10-03 23:27:54 -0400

    Fix test

--- Tp-Backend/src/main/java/com/example/tpbackend/repository/CandidatureRepository.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/repository/CandidatureRepository.java
@@ -2,12 +2,13 @@ package com.example.tpbackend.repository;
 
 import com.example.tpbackend.DTO.candidature.CandidatureGetDTO;
 import com.example.tpbackend.models.Candidature;
+import com.example.tpbackend.models.utilisateur.etudiant.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CandidatureRepository extends JpaRepository<Candidature, Integer> {
-    @Query("SELECT lettre_motivation,fileName,offreStage,student,cvStudent FROM Candidature WHERE student.matricule = ?1")
+    @Query(value = "SELECT * FROM Candidature WHERE student_candidat = ?1",nativeQuery = true)
     List<Candidature> getAllCandidaturesByMatricule(String matricule);
 }

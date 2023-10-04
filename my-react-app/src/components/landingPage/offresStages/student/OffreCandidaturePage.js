b2e567042ecfd7d893d80570d8e6378d297290c9
Philippe Vallerand <pvallerand1@gmail.com>
2023-10-04 10:45:33 -0400

    EQ2-69-fix front end fetch and data management

--- my-react-app/src/components/landingPage/offresStages/student/OffreCandidaturePage.js
+++ my-react-app/src/components/landingPage/offresStages/student/OffreCandidaturePage.js
@@ -8,27 +8,43 @@ import Box from '@mui/material/Box';
 import OffreDescription from "../OffreDescription";
 
 const OffreCandidaturePage = () => {
-    const [offres, setOffres] = useState([]);
+    const [candidature, setCandidature] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [shouldRefetch] = useState(false);
 
     useEffect(() => {
         const savedMatricule = localStorage.getItem("loggedInUserMatricule");
-        const fetchOffreList = async () => {
-            try {
-                const response = await fetch(`http://localhost:8081/api/v1/stages/getMesCandidatures?matricule=${savedMatricule}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setOffres(data);
-                } else {
-                    console.error("Failed to fetch data");
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
+        fetch(
+            `http://localhost:8081/api/v1/student/getMesCandidatures/${savedMatricule}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
             }
-        };
+        ).catch((error) => {
+            console.error("Error:", error);
+        }).then(
+            async (response) => {
+                const data = await response.json();
+                console.log(response.status)
+                try{
+                    console.log(response.status)
+                }
+                catch (e) {
+                    console.log(e)
+                }
+                setCandidature(data)
+                setIsLoading(false);
+            }
+        );
 
-        fetchOffreList();
     }, [shouldRefetch]);
+
+    if (isLoading) {
+        return <div>Chargement...</div>;
+    }
+
 
     return (
         <div>
@@ -39,21 +55,21 @@ const OffreCandidaturePage = () => {
             </div>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container>
-                    {offres.map((offre, index) => (
+                    {candidature.map((candidature, index) => (
                         <div key={index} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
                             <Grid item xs={12} sm={12} md={4} lg={4}>
                                 <Card style={{ width: '30rem', margin: '5px' }}>
                                     <Card.Body>
                                         <Card.Title>
-                                            {offre.titre}
+                                            {candidature.offreStageDTO.titre}
                                         </Card.Title>
-                                        <OffreDescription offre={offre}/>
+                                        <OffreDescription offre={candidature.offreStageDTO}/>
                                     </Card.Body>
                                     <ListGroup className="list-group-flush">
-                                        <ListGroup.Item>Salaire: {offre.salaire}$</ListGroup.Item>
-                                        <ListGroup.Item>Programme: {offre.studentProgram}</ListGroup.Item>
-                                        <ListGroup.Item>Date de début: {offre.dateDebut}</ListGroup.Item>
-                                        <ListGroup.Item>Date de fin: {offre.dateFin}</ListGroup.Item>
+                                        <ListGroup.Item>Salaire: {candidature.offreStageDTO.salaire}$</ListGroup.Item>
+                                        <ListGroup.Item>Programme: {candidature.offreStageDTO.studentProgram}</ListGroup.Item>
+                                        <ListGroup.Item>Date de début: {candidature.offreStageDTO.dateDebut}</ListGroup.Item>
+                                        <ListGroup.Item>Date de fin: {candidature.offreStageDTO.dateFin}</ListGroup.Item>
                                     </ListGroup>
                                 </Card>
                             </Grid>

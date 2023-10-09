e5a83576bd6f7174fa7ff4ecfd973769906bf571
Danil <dextwelwe@gmail.com>
2023-10-09 12:26:07 -0400

    backend fonctionnel, front end terminer, api terminer

--- my-react-app/src/components/landingPage/offresStages/informationEtudiantPostule.js
+++ my-react-app/src/components/landingPage/offresStages/informationEtudiantPostule.js
@@ -1,65 +1,43 @@
 import React from 'react'
+import { useState } from "react";
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './informationEtudiantPostule.css'
 import { useNavigate } from "react-router-dom";
 import {useLocation} from 'react-router-dom';
+import Modal from "../GestionnaireHomePage/Vetocv/Modal";
+
 export default function InformationEtudiantPostule({listeEtudiants}) {
   const navigate = useNavigate();
   const location = useLocation();
-  {/*   Pour le api
   listeEtudiants = location.state.listeEtudiants
-*/}
+  const [openModal, setOpenModal] = useState(false);
+  const [openModalLettre, setOpenModalLetttre] = useState(false)
+  const [student, setStudent] = useState(null);
 
-  listeEtudiants = [{
-    nom: 'mosk0',
-    prenom: 'danil0aaaaaaaaaaaaaaaaa21312312321312312321321312321',
-    adresseCourr : 'dex@gmail.qwe',
-    numTel : '438-888-88',
-    lettre_de_motivation: 'abc',
-    resume: 'bcd',
-  },
-  {
-    nom: 'mosk1',
-    prenom: 'danil1',
-    adresseCourr : 'dex@gmail.qwe',
-    numTel : '438-888-88',
-    lettre_de_motivation: 'abc',
-    resume: 'bcd',
-  },{
-    nom: 'mosk2',
-    prenom: 'danil2',
-    adresseCourr : 'dex@gmail.qwe',
-    numTel : '438-888-88',
-    lettre_de_motivation: 'abc',
-    resume: 'bcd',
-  },
-  {
-    nom: 'mosk2',
-    prenom: 'danil2',
-    adresseCourr : 'dex@gmail.qwe',
-    numTel : '438-888-88',
-    lettre_de_motivation: 'abc',
-    resume: 'bcd',
-  }
-]
-
+function handleMontrerCv(student){
+setOpenModal(!openModal)
+setStudent(student)
+}
 function handleRetour(){
-  navigate('/EmployeurHomePage')
+navigate('/EmployeurHomePage')
+}
+function handleMontrerLettre(student){
+setOpenModalLetttre(!openModalLettre)
+setStudent(student)
 }
   return (
-    <div>
-  
+    <div className='mt-5'>
     <div className='rootInfo'>
       <div className='divFormInfo'>
       <table>
-      <caption> <h1 className='text-center'>INFORMATIONS ÉTUDIANTS POSTULÉS</h1> </caption>
+      <caption> <h1 className='text-center'>LISTE D'ÉTUDIANTS POSTULÉS</h1> </caption>
       <thead>
       <tr>
       <th scope="col" className='headerElement'>NOM</th>
       <th scope='col' className='headerElement'>PRENOM</th>
       <th scope='col' className='headerElement'>ADRESSE COURRIEL</th>
       <th scope='col'className='headerElement'>NUMERO DE TELEPHONE</th>
-      <th scope='col'className='headerElement'>RESUME</th>
+      <th scope='col'className='headerElement text-center'>RESUME</th>
       <th scope='col'className='headerElement'>LETTRE DE MOTIVATION</th>
       <th scope='col'className='headerElement text-center'>ACTION</th>
       </tr>  
@@ -67,19 +45,25 @@ function handleRetour(){
       <tbody>
       {listeEtudiants.map((etudiant, i) => (
       <tr key={i} > 
-      <td data-label="NOM" scope="row" className='headerElement breakWord'>{etudiant.nom}</td>
-      <td  data-label="PRENOM" className='headerElement breakWord'><p className='breakWord'>{etudiant.prenom}</p></td>
-      <td data-label="ADRESSE COURRIEL"className='headerElement'>{etudiant.adresseCourr}</td>
-      <td data-label="NUMERO DE TELEPHONE"className='headerElement'>{etudiant.numTel}</td>
-      <td data-label="LETTRE DE MOTIVATION"className='headerElement'>{etudiant.lettre_de_motivation}</td>
-      <td data-label="RESUME"className='headerElement'>{etudiant.resume}</td>
-      <td><button className='btn btn-warning me-3'>CONVOQUER</button></td>
+      <td data-label="NOM" scope="row" className='headerElement breakWord'>{etudiant.student.firstName}</td>
+      <td  data-label="PRENOM" className='headerElement breakWord'>{etudiant.student.lastName}</td>
+      <td data-label="ADRESSE COURRIEL"className=' headerElement'>{etudiant.student.email}</td>
+      <td data-label="NUMERO DE TELEPHONE"className=' headerElement'>{etudiant.student.phoneNumber}</td>
+      <td data-label="RESUME"className='headerElement '><button className='btn btn-info p-3' onClick={()=>handleMontrerCv(etudiant)}>RESUME</button>  </td>
+      <td data-label="LETTRE DE MOTIVATION"className='headerElement'><button className='btn btn-info p-lg-1 p-md-1 p-sm-3' onClick={()=> handleMontrerLettre(etudiant)}>LETTRE MOTIVATION</button></td>
+      <td><button className='btn btn-warning  p-3'>CONVOQUER</button></td>
       </tr>
     ))}
     </tbody>
     </table>
-    <div className='d-flex justify-content-end me-3 mt-5'>
-    <button className='btn btn-danger ' onClick={handleRetour}>RETOUR </button>
+    {openModal &&
+    <Modal cv={student.cvStudent.file_cv} fileName={student.cvStudent.fileName} onClose={handleMontrerCv} />
+    }
+    {openModalLettre &&
+      <Modal cv={student.lettreMotivation} fileName={student.fileName} onClose={handleMontrerLettre} />
+    }
+    <div className='d-flex justify-content-end mt-5 p-3'>
+    <button className='btn btn-danger p-2 ' onClick={handleRetour}>RETOUR</button>
     </div>
     </div>
     </div>

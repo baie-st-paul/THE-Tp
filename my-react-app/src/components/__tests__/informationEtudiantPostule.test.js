bdf1ea35515d892ab649037a576ac885998ab3b4
lina <2033162@claurendeau.qc.ca>
2023-10-10 18:07:30 -0400

    initialisation jest test, creation dossier tests

---
+++ my-react-app/src/components/__tests__/informationEtudiantPostule.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import InformationEtudiantPostule from '../landingPage/offresStages/student/info/informationEtudiantPostule';
+
+
+const testList = {
+    
+
+
+}
--- my-react-app/src/components/landingPage/offresStages/student/info/informationEtudiantPostule.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import InformationEtudiantPostule from './informationEtudiantPostule';
-
-
-const testList = {
-    
-
-
-}

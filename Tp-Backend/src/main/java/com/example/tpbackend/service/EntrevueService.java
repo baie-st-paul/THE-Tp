a95678516b33f72a721ed08c2e4a74edb95dcb0b
yskuridov <yskuridov@gmail.com>
2023-10-11 16:38:21 -0400

    Tests fonctionnels

--- Tp-Backend/src/main/java/com/example/tpbackend/service/EntrevueService.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/service/EntrevueService.java
@@ -4,7 +4,10 @@ import com.example.tpbackend.DTO.EntrevueDTO;
 import com.example.tpbackend.DTO.utilisateur.employeur.EmployerGetDTO;
 import com.example.tpbackend.DTO.utilisateur.student.StudentGetDTO;
 import com.example.tpbackend.models.Entrevue;
+import com.example.tpbackend.models.utilisateur.employeur.Employer;
 import com.example.tpbackend.repository.EntrevueRepository;
+import com.example.tpbackend.repository.utilisateur.EmployerRepository;
+import com.example.tpbackend.repository.utilisateur.StudentRepository;
 import com.example.tpbackend.service.utilisateur.EmployerService;
 import com.example.tpbackend.service.utilisateur.StudentServices;
 import lombok.NoArgsConstructor;
@@ -17,15 +20,23 @@ public class EntrevueService {
 
     @Autowired
     private EntrevueRepository entrevueRepository;
-    @Autowired
-    private EmployerService employerService;
-    @Autowired
-    private StudentServices studentServices;
 
-    public EntrevueDTO createEntrevue(EntrevueDTO entrevueDTO) {
-        Entrevue entrevue = entrevueDTO.toEntrevue();
-        entrevue.setEmployer(EmployerGetDTO.fromEmployerDTO(employerService.getEmployerById(Long.valueOf(entrevueDTO.getIdEmployeur()))));
-        entrevue.setStudent(StudentGetDTO.fromStudentDTO(studentServices.getStudentByMatricule(entrevueDTO.getIdEtudiant())));
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    public EntrevueDTO createEntrevue(EntrevueDTO entrevueDTO) throws Exception {
+        Entrevue entrevue = new Entrevue();
+        entrevue.setId(1L);
+        entrevue.setDescription(entrevueDTO.getDescription());
+        entrevue.setDateHeure(entrevueDTO.getDateHeure());
+        entrevue.setStatus(Entrevue.Status.valueOf(entrevueDTO.getStatus()));
+        entrevue.setEmployer(employerRepository.findEmployerById(Long.parseLong(entrevueDTO.getIdEmployeur())));
+        entrevue.setStudent(studentRepository.findByMaticule(entrevueDTO.getIdEtudiant()));
         return entrevueRepository.save(entrevue).toEntrevueDTO();
     }
+
 }

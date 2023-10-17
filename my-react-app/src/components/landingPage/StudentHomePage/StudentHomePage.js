b422f7638638b46784827f005b80730e5b8a19b2
Ivan Jimenez <ivanjr1802@gmail.com>
2023-10-16 23:27:22 -0400

    Merge fix

--- my-react-app/src/components/landingPage/StudentHomePage/StudentHomePage.js
+++ my-react-app/src/components/landingPage/StudentHomePage/StudentHomePage.js
@@ -5,17 +5,11 @@ import { useUser } from "../../../Providers/UserProvider";
 import {Nav, Navbar} from "react-bootstrap";
 import OffresPageStudent from "../offresStages/student/candidature/OffrePageStudent";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-<<<<<<< HEAD
 import {faFileUpload, faBriefcase, faEnvelope, faHome} from '@fortawesome/free-solid-svg-icons';
-import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 import SectionEntrevue from "./SectionViewEntrevue/SectionEntrevue";
 import Dashboard from "./DashBoard/Dashboard";
-
-=======
-import {faFileUpload, faBriefcase, faPortrait, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import OffreCandidaturePage from "../offresStages/student/candidature/OffreCandidaturePage";
 import { useNavigate } from "react-router-dom";
->>>>>>> origin/EQ2-17-employeur_convoquer_etudiant_entrevue
 
 const StudentHomePage = () => {
     const { loggedInUser, setLoggedInUser } = useUser();
@@ -54,56 +48,21 @@ const StudentHomePage = () => {
         case "offre-page-student":
             contentToRender = <OffresPageStudent/>;
             break;
-<<<<<<< HEAD
         case "section-entrevue":
             contentToRender = <SectionEntrevue/>
             break;
-        default:
-          contentToRender = (
-              <Dashboard/>);
-            /*<Container fluid>
-              <Row>
-                <Col xs={4}>
-                  <div className="sidebar">
-                    <ListGroup>
-                      {articles.map((article, index) => (
-                        <ListGroup.Item action href={`#article-${index + 1}`} key={index}>
-                          {article}
-                        </ListGroup.Item>
-                      ))}
-                    </ListGroup>
-                  </div>
-                </Col>
-                <Col xs={8}>
-                  {articles.map((article, index) => (
-                    <Card className="mb-4" key={index} id={`article-${index + 1}`}>
-                      <Card.Header as="h1">{article}</Card.Header>
-                      <Card.Body>
-                        <Card.Title as="h3">Introduction</Card.Title>
-                        <Card.Text className="light-bg">
-                          Contenu de {article}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  ))}
-                </Col>
-              </Row>
-            </Container>
-          );*/
-          break;
-=======
         case "offre-page-candidature":
             contentToRender = <OffreCandidaturePage/>;
             break;
         default:
-            contentToRender = <div>Please select a section.</div>;
+            contentToRender = (
+                <Dashboard/>);
+
             break;
->>>>>>> origin/EQ2-17-employeur_convoquer_etudiant_entrevue
     }
 
     return (
         <div className="student-homepage">
-<<<<<<< HEAD
             <Navbar bg="dark" className="navbar-dark" expand="lg">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -117,41 +76,16 @@ const StudentHomePage = () => {
                             <li className="nav-item">
                                 <button className="nav-link" onClick={() => setActiveContent('file-uploader')}>
                                     <FontAwesomeIcon icon={faFileUpload} style={{ marginRight: '2px' }}/>CV
-=======
-            <Navbar  className="navbar-dark navbarClass border border-dark" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav navbar-fluid " />
-                <Navbar.Collapse id="basic-navbar-nav">               
-                    <Nav>
-                        <ul className="navbar-nav px-2">
-                            <li className="nav-item navbarbutton">
-                                <button className="nav-link" onClick={() => setActiveContent('file-uploader')}>
-                                    <FontAwesomeIcon icon={faFileUpload} style={{ marginRight: '10px' }}/> CV
->>>>>>> origin/EQ2-17-employeur_convoquer_etudiant_entrevue
                                 </button>
                             </li>
                             <li className="nav-item navbarbutton">
                                 <button className="nav-link" onClick={() => handleButtonClick('offre-page-student')}>
-<<<<<<< HEAD
                                     <FontAwesomeIcon icon={faBriefcase} style={{ marginRight: '2px' }}/> Offres
                                 </button>
                             </li>
                             <li className="nav-item">
                                 <button className="nav-link" onClick={() => handleButtonClick('section-entrevue')}>
                                     <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '2px' }}/> Section Entrevue
-=======
-                                    <FontAwesomeIcon icon={faBriefcase} style={{ marginRight: '10px' }}/>Offres
-                                </button>
-                            </li>
-                            <li className="nav-item navbarbutton">
-                                <button className="nav-link" onClick={() => handleButtonClick('offre-page-candidature')}>
-                                    <FontAwesomeIcon icon={faPortrait} style={{ marginRight: '10px' }}/> Mes candidatures
-                                </button>
-                            </li>
-                            <li className="nav-item navbarbutton deconnecter">
-                                <button className="nav-link" onClick={() => handleDisconnect()}>
-                                    <FontAwesomeIcon icon={faArrowRight} style={{marginTop:'5px', marginRight: '10px' }}/>
-                                    Se déconnecter
->>>>>>> origin/EQ2-17-employeur_convoquer_etudiant_entrevue
                                 </button>
                             </li>
                         </ul>   
@@ -159,10 +93,7 @@ const StudentHomePage = () => {
                 </Navbar.Collapse>
           </Navbar>
           <div className="container content-container mt-4">
-<<<<<<< HEAD
-=======
             <h2>Étudiant</h2>
->>>>>>> origin/EQ2-17-employeur_convoquer_etudiant_entrevue
             {contentToRender}
           </div>
         </div>

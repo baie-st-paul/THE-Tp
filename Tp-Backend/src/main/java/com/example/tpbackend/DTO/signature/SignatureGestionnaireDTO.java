cb94b03b9b7fcf45e9d5b3b8fdba346dca6abc5f
yskuridov <yskuridov@gmail.com>
2023-11-13 12:40:00 -0500

    CRU methods for the signature of the gestionnaire

--- Tp-Backend/src/main/java/com/example/tpbackend/DTO/signature/SignatureGestionnaireDTO.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/DTO/signature/SignatureGestionnaireDTO.java
@@ -1,6 +1,7 @@
 package com.example.tpbackend.DTO.signature;
 
 
+import com.example.tpbackend.models.signature.SignatureGestionnaire;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,10 @@ import lombok.NoArgsConstructor;
 public class SignatureGestionnaireDTO {
     private String gestionnaireMatricule;
     private String imageLink;
+
+    public SignatureGestionnaire toSignatureGestionnaire() {
+        return new SignatureGestionnaire(
+                imageLink
+        );
+    }
 }
--- Tp-Backend/src/main/java/com/example/tpbackend/repository/signature/SignatureGestionnaireRepository.java
+++ Tp-Backend/src/main/java/com/example/tpbackend/repository/signature/SignatureGestionnaireRepository.java
@@ -1,11 +1,11 @@
 package com.example.tpbackend.repository.signature;
 
 import com.example.tpbackend.models.signature.SignatureGestionnaire;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SignatureGestionnaireRepository {
-    SignatureGestionnaire findByGestionnaire_Matricule();
-
+public interface SignatureGestionnaireRepository extends JpaRepository<SignatureGestionnaire, Long> {
+    SignatureGestionnaire findByGestionnaire_Matricule(String matricule);
 
 }

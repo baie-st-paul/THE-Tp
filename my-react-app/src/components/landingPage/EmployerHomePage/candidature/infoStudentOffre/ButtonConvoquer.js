4a060b7ce4a69d14864542245e657217b5976544
Philippe Vallerand <pvallerand1@gmail.com>
2023-11-23 18:56:08 -0500

    EQ2-273-changement test

--- my-react-app/src/components/landingPage/EmployerHomePage/candidature/infoStudentOffre/ButtonConvoquer.js
+++ my-react-app/src/components/landingPage/EmployerHomePage/candidature/infoStudentOffre/ButtonConvoquer.js
@@ -23,45 +23,36 @@ export default function ButtonConvoquer({matricule, offre, entrevues , setModal,
 
     return (
         <>
-            {offre.status === "Interview" ?
+            {isPresent.length > 0 /*|| isPresent[0].status !== 'Refusee'*/ ?
                 <>
-                    {isPresent.length > 0 /*|| isPresent[0].status !== 'Refusee'*/ ?
-                        <>
-                            { isPresent[0].status !== 'Refusee' ?
-                                <td data-label="ENTREVUE" scope="row" className='headerElement breakWord h6 pe-3'>
-                                    {isPresent[0].dateHeure}
-                                    {
-                                        isPresent[0].status === 'Acceptee' ?
-                                            <span className="badge bg-success ms-2">Acceptée</span>
-                                            :
-                                            <span className="badge bg-warning text-dark ms-2">En attente</span>
-                                    }
-                                </td> :
-                                <td data-label="ENTREVUE" className='headerElement h6 px-3 pe-0' >
-                                    <button title="RECONVOQUER"  className='btn btn-warning pb-5 pt-0 text-start ' style={{height : "58px", width: '105px'}}
-                                            onClick={()=>{
-                                                handleConvoquerEntrevue({candidatureId})
-                                                entrevueToModify(isPresent[0])
-                                            }}>
-                                        Entrevue Refusée
-                                    </button>
-                                </td>
+                    { isPresent[0].status !== 'Refusee' ?
+                        <td data-label="ENTREVUE" scope="row" className='headerElement breakWord h6 pe-3'>
+                            {isPresent[0].dateHeure}
+                            {
+                                isPresent[0].status === 'Acceptee' ?
+                                    <span className="badge bg-success ms-2">Acceptée</span>
+                                    :
+                                    <span className="badge bg-warning text-dark ms-2">En attente</span>
                             }
-                        </>
-                         :
-                        <td data-label="ENTREVUE" className='headerElement h6 px-3 pe-0  '>
-                            <button title="CONVOQUER" className='btn btn-primary pb-5 pt-0 text-start' style={{height : "58px", width: '105px' }}
-                                    onClick={()=> handleConvoquerEntrevue({candidatureId})}>
-                                Convoquer
+                        </td> :
+                        <td data-label="ENTREVUE" className='headerElement h6 px-3 pe-0' >
+                            <button title="RECONVOQUER"  className='btn btn-warning pb-5 pt-0 text-start ' style={{height : "58px", width: '105px'}}
+                                    onClick={()=>{
+                                        handleConvoquerEntrevue({candidatureId})
+                                        entrevueToModify(isPresent[0])
+                                    }}>
+                                Entrevue Refusée
                             </button>
                         </td>
                     }
-                </> :
-                <>
-                    <td data-label="ENTREVUE" className='headerElement h6 px-3 pe-0  '>
-                        <span>Entrevue non-disponible</span>
-                    </td>
                 </>
+                 :
+                <td data-label="ENTREVUE" className='headerElement h6 px-3 pe-0  '>
+                    <button title="CONVOQUER" className='btn btn-primary pb-5 pt-0 text-start' style={{height : "58px", width: '105px' }}
+                            onClick={()=> handleConvoquerEntrevue({candidatureId})}>
+                        Convoquer
+                    </button>
+                </td>
             }
         </>
     )

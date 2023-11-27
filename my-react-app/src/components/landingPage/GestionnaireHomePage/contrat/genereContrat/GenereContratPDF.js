a0e28d78bef875414a038364ac8552369e139263
lina <2033162@claurendeau.qc.ca>
2023-11-27 17:38:23 -0500

    essayer de faire css pdf contrat mais sort pas comme l'original

--- my-react-app/src/components/landingPage/GestionnaireHomePage/contrat/genereContrat/GenereContratPDF.js
+++ my-react-app/src/components/landingPage/GestionnaireHomePage/contrat/genereContrat/GenereContratPDF.js
@@ -4,10 +4,27 @@ import React from "react";
 const styles = StyleSheet.create({
     body: { padding: 10 },
     title: { fontSize: 20, textAlign: 'center', marginBottom: 10, fontWeight: 'bold' },
+    littleTitle: { fontSize: 12, textAlign: 'center', marginBottom: 10 },
     subtitle: { fontSize: 15, marginTop: 10, marginBottom: 5, fontWeight: 'bold' },
+    text: { margin: 10, fontSize: 11, textAlign: 'center' },
+    section: { marginBottom: 25, textAlign: 'center' },
+    signatureImage: { width: 100, height: 50, margin: '10px 0' },
+    errorMessage: { color: 'red', fontSize: 10, marginTop: 5 },
+    tableRow: {
+        margin: "auto",
+        flexDirection: "row"
+    },
+    tableCol: {
+        width: "25%",
+        borderStyle: "solid",
+        borderWidth: 1,
+    },
+    tableCell: {
+        margin: "auto",
+        marginTop: 5,
+        fontSize: 10
+    },
     header: { fontSize: 12, fontWeight: 'bold', marginBottom: 2 },
-    text: { margin: 5, fontSize: 12, textAlign: 'justify' },
-    section: { marginBottom: 10, textAlign: 'center', border: '1px solid black', padding: 10, fontSize: 15 },
     footer: { fontSize: 7, textAlign: 'left', marginTop: 10 },
     input: { marginBottom: 3 },
     textarea: { marginBottom: 3, height: 100 },
@@ -15,11 +32,6 @@ const styles = StyleSheet.create({
     radio: { margin: 3 },
     select: { marginBottom: 3 },
     button: { marginTop: 10 },
-    table: { display: "table", width: "auto", borderStyle: "solid", borderWidth: 1, borderRightWidth: 0, borderBottomWidth: 0 },
-    tableRow: { margin: "auto", flexDirection: "row" },
-    tableColHeader: { width: "25%", borderStyle: "solid", borderBottomWidth: 1, borderLeftWidth: 1 },
-    tableCol: { width: "25%", borderStyle: "solid", borderLeftWidth: 1 },
-
     footerInfo: {
         textAlign: 'center',
         fontSize: 10,
@@ -30,67 +42,74 @@ const styles = StyleSheet.create({
         color: 'black',
         fontWeight: 'bold',
         textAlign: 'left',
-    },
-
-    signatureImage: {
-        width: 100,
-        height: 50,
-        margin: '10px 0',
     }
 });
 
 const GenereContratPDF = ({formData}) => (
     <Document>
         <Page size="A4" style={styles.body}>
-            <Text style={styles.title}>ENTENTE DE STAGE INTERVENUE ENTRE LES PARTIES SUIVANTES</Text>
-
             <View style={styles.section}>
+                <Text style={styles.title}>ENTENTE DE STAGE INTERVENUE ENTRE LES PARTIES SUIVANTES</Text>
+
                 <Text style={styles.subtitle}>Dans le cadre de la formule ATE, les parties citées ci-dessous :</Text>
 
-                <Text style={styles.text}>Le gestionnaire de stage, {formData.nomGestionnaire}</Text>
-                <Text style={styles.text}>et</Text>
-                <Text style={styles.text}>L'employeur, {formData.nomEmployeur}</Text>
-                <Text style={styles.text}>et</Text>
-                <Text style={styles.text}>L'étudiant(e)', {formData.nomEtudiant}</Text>
+                <Text style={styles.littleTitle}>Le gestionnaire de stage, <b>{formData.nomGestionnaire}</b></Text>
+                <Text style={styles.littleTitle}><b>et</b></Text>
+                <Text style={styles.littleTitle}>L'employeur, <b>{formData.nomEmployeur}</b></Text>
+                <Text style={styles.littleTitle}><b>et</b></Text>
+                <Text style={styles.littleTitle}>L'étudiant(e)', <b>{formData.nomEtudiant}</b></Text>
 
                 <Text style={styles.subtitle}>Conviennent des conditions de stage suivantes :</Text>
-            </View>
 
-            <View style={styles.tableRow}>
-                <View style={styles.tableCol}>
-                    <Text style={styles.tableCell}>ENDROIT DU STAGE</Text>
-                </View>
-
-                <View style={styles.tableCol}>
-                    <Text style={styles.tableCell}>DUREE DU STAGE</Text>
-                </View>
-
-                <View style={styles.tableCol}>
-                    <Text style={styles.tableCell}>HORAIRE DE TRAVAIL</Text>
-                </View>
-
-                <View style={styles.tableCol}>
-                    <Text style={styles.tableCell}>SALAIRE</Text>
-                </View>
-
-                <View style={styles.tableRow}>
-                    <View style={styles.tableCol}>
-                        <Text style={styles.tableCell}>Adresse: {formData.offreLieuStage}</Text>
+                <View style={styles.section}>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}><b>ENDROIT DU STAGE</b></Text>
+                        </View>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Adresse: {formData.offreLieuStage}</Text>
+                        </View>
                     </View>
 
-                    <View style={styles.tableCol}>
-                        <Text style={styles.tableCell}>Date de début: {formData.dateDebut}</Text>
-                        <Text style={styles.tableCell}>Date de fin: {formData.dateFin}</Text>
-                        <Text style={styles.tableCell}>Nombre total de semaines: {formData.nbTotalSemaines}</Text>
+
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}><b>DUREE DU STAGE</b></Text>
+                        </View>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Date de début: {formData.dateDebut}</Text>
+                            <Text style={styles.tableCell}>Date de fin: {formData.dateFin}</Text>
+                            <Text style={styles.tableCell}>Nombre total de semaines: {formData.nbTotalSemaines}</Text>
+                        </View>
                     </View>
 
-                    <View style={styles.tableCol}>
-                        <Text style={styles.tableCell}>Horaire de travail: {`${formData.startWorkHours} à ${formData.endWorkHours}`}</Text>
-                        <Text style={styles.tableCell}>Nombre total d'heures par semaine: {formData.nbTotalHeureParSemaine}</Text>
+
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}><b>HORAIRE DE TRAVAIL</b></Text>
+                        </View>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Horaire de travail: {`De ${formData.startWorkHours}h à ${formData.endWorkHours}h`}</Text>
+                            <Text style={styles.tableCell}>Nombre total d'heures par semaine{formData.nbTotalHeureParSemaine}h</Text>
+                        </View>
                     </View>
 
-                    <View style={styles.tableCol}>
-                        <Text style={styles.tableCell}>Salaire horaire: {formData.salaireHoraire}</Text>
+
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}><b>SALAIRE</b></Text>
+                        </View>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <View style={styles.tableCol}>
+                            <Text style={styles.tableCell}>Salaire horaire: {formData.salaireHoraire}$/h</Text>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -146,10 +165,6 @@ const GenereContratPDF = ({formData}) => (
                         <View style={styles.tableCol}>
                             <Text style={styles.tableCell}>{formData.nomEtudiant}</Text>
                         </View>
-
-                        <View style={styles.tableCol}>
-                            <Text style={styles.tableCell}>Date</Text>
-                        </View>
                     </View>
                 </View>
 
@@ -176,10 +191,6 @@ const GenereContratPDF = ({formData}) => (
                         <View style={styles.tableCol}>
                             <Text style={styles.tableCell}>{formData.nomEmployeur}</Text>
                         </View>
-
-                        <View style={styles.tableCol}>
-                            <Text style={styles.tableCell}>Date</Text>
-                        </View>
                     </View>
                 </View>
 
@@ -205,10 +216,6 @@ const GenereContratPDF = ({formData}) => (
                     <View style={styles.tableRow}>
                         <View style={styles.tableCol}>
                             <Text style={styles.tableCell}>{formData.nomGestionnaire}</Text>
-                        </View>
-
-                        <View style={styles.tableCol}>
-                            <Text style={styles.tableCell}>Date</Text>
                         </View>
                     </View>
                 </View>

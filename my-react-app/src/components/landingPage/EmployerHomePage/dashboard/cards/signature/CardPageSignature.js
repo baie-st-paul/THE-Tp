664c9541831ecb538b0b41a6d7f5564982bdf2fa
lina <2033162@claurendeau.qc.ca>
2023-12-18 10:15:12 -0500

    gestionnaire home page debut re design

--- my-react-app/src/components/landingPage/EmployerHomePage/dashboard/cards/signature/CardPageSignature.js
+++ my-react-app/src/components/landingPage/EmployerHomePage/dashboard/cards/signature/CardPageSignature.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./CardPageSignature.css"
+import error_cv_signature from "../../../../../../images/error_cv_signature.gif"
 
 const CardPageSignature = () => {
     return (
-        <div className="container text-center mt-5" style={{justifyContent: "center", display: "flex"}}>
-            <div style={{width: "500px"}}>
-                <img
-                    src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
-                    alt="Ajouter une signature"
-                    className="img-fluid"
-                />
-                <div>Ajouter une signature !</div>
-                <p className="lead">Le tableau de bord sera affiché par la suite</p>
-            </div>
+        <div className="text-center m-0 p-0"
+             style={{justifyContent: "center"}}>
+            <img style={{width: "400px"}}
+                src={error_cv_signature}
+                alt="Ajouter une signature"
+                className="img-fluid"
+            />
+            <div>Ajouter une signature !</div>
+            <p className="lead">Le tableau de bord sera affiché par la suite</p>
         </div>
     )
 }
